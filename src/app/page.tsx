26b710cb6fb355a150a48215831ad5bc00dff8bf
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2024-08-17 16:46:30 -0300

    feat: finished sidebar desktop with card in dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,80 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { BadgeDollarSign, DollarSign, Percent, Users } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
-     <h1>Projeto Dashboard</h1>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800">
+                Total vendas
+              </CardTitle>
+              <DollarSign className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              Total vendas em 30 dias
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+              <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+            </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800">
+                Novos clientes
+              </CardTitle>
+              <Users className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              Total clientes em 30 dias
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+              <p className="text-base sm:text-lg font-bold">249</p>
+            </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800">
+                Pedidos hoje
+              </CardTitle>
+              <Percent className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              Total pedidos hoje
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+              <p className="text-base sm:text-lg font-bold">79</p>
+            </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800">
+                Total pedidos
+              </CardTitle>
+              <BadgeDollarSign className="ml-auto w-4 h-4" />
+            </div>
+            <CardDescription>
+              Total pedidos em 30 dias
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+              <p className="text-base sm:text-lg font-bold">2500</p>
+            </CardContent>
+        </Card>
+
+      </section>
     </main>
   );
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,103 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { Home, Package, PanelBottom, Settings2, ShoppingBag, Users } from "lucide-react";
+import { Home, LogOut, Package, PanelBottom, Settings2, ShoppingBag, Users } from "lucide-react";
+import { Tooltip, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+import { TooltipContent } from "@radix-ui/react-tooltip";
 
 export function Sidebar() {
     return (
         <div className="flex flex-col w-full bg-muted/40">
+
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex flex-col">
+                <nav className="flex flex-col gap-4 items-center py-5 px-2">
+                    <TooltipProvider>
+                        <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center bg-primary text-primary-foreground rounded-full">
+                            <Package className="w-4 h-4" />
+                            <span className="sr-only">Dashboard Avatar</span>
+                        </Link>
+
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Home className="w-4 h-4" />
+                                    <span className="sr-only">Início</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Início
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <ShoppingBag className="w-4 h-4" />
+                                    <span className="sr-only">Pedidos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Pedidos
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Package className="w-4 h-4" />
+                                    <span className="sr-only">Produtos</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Produtos
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Users className="w-4 h-4" />
+                                    <span className="sr-only">Clientes</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Clientes
+                            </TooltipContent>
+                        </Tooltip>
+
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <Settings2 className="w-4 h-4" />
+                                    <span className="sr-only">Configurações</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Configurações
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                </nav>
+                <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link href="#" className="flex h-9 w-9 shrink-0 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground">
+                                    <LogOut className="w-4 h-4" />
+                                    <span className="sr-only">Sair</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">
+                                Sair
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                </nav>
+
+            </aside>
 
             <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                 <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
@@ -71,8 +163,8 @@ export function Sidebar() {
                                     className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
                                     prefetch={false}
                                 >
-                                    <Settings2 className="w-5 h-5" />
-                                    Configurações
+                                    <LogOut className="w-5 h-5" />
+                                    Sair
                                 </Link>
                             </nav>
                         </SheetContent>
@@ -80,7 +172,7 @@ export function Sidebar() {
                     <h2>Menu</h2>
                 </header>
             </div>
-            
+
         </div>
     );
 }
