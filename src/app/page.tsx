eca6935bf4eda2cab861a9f12fe0b19b121f4f75
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2024-08-17 17:51:06 -0300

    feat: card new client in dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ChartOverview } from "@/components/chart";
+import { Sales } from "@/components/sales";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { BadgeDollarSign, DollarSign, Percent, Users } from "lucide-react";
 
@@ -77,10 +78,10 @@ export default function Home() {
 
       </section>
 
-      <section className="mt-4 flex flex-col md:flex-row fap-4">
+      <section className="mt-4 flex flex-col md:flex-row gap-4">
 
        <ChartOverview />
-       
+       <Sales />
       </section>
     </main>
   );
---
+++ src/components/sales.tsx
@@ -0,0 +1,31 @@
+import { CircleDollarSign } from "lucide-react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export function Sales() {
+    return ( 
+        <Card className="flex-1">
+             <CardHeader>
+                <div className="flex items-center justify-center">
+                    <CardTitle className="text-lg sm:text-xl text-gray-800">Últimos clientes</CardTitle>
+                    <CircleDollarSign className="ml-auto w-4 h-4" />
+                </div>
+                <CardDescription className="">Novos clientes nas últimas 24 horas</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {Array.from({ length: 5 }, (_, index) => (
+                    <article className="flex items-center gap-2 border-b py-2" key={index}>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/peedrohenrique.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <p className="text-sm font-semibold">Pedro Henrique</p>
+                            <span className="text-xs sm:text-sm text-muted-foreground">test@test.com</span>
+                        </div>
+                    </article>
+                ))}        
+            </CardContent>
+        </Card>
+     );
+}
